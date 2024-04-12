54adbcefac46f9b13c805085c910b18a0bbb392f
SDSamarasinghe <it20605902@my.sliit.lk>
2024-04-12 11:18:27 +0530

    Add dotenv dependency and configure CORS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+require('dotenv').config();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  if (process.env.APP_ENV !== 'production') {
+    app.enableCors({
+      allowedHeaders: '*',
+      origin: '*',
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      allowedHeaders: '*',
+      origin: 'http://localhost:4200',
+    });
+  }
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
