37cbf230e60c38b165b81e814321caf051c95df2
SDSamarasinghe <it20605902@my.sliit.lk>
2024-04-13 11:13:32 +0530

    Add dependencies for @nestjs/config, @nestjs/microservices, amqp-connection-manager, and amqplib

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { config } from 'dotenv';
 config();
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL), ProductModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URL),
+    ProductModule,
+    ClientsModule.register([
+      {
+        name: 'INVENTORY_MICROSERVICE',
+        transport: Transport.TCP,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  MicroserviceOptions,
+  Transport,
+  ClientProxy,
+  ClientProxyFactory,
+} from '@nestjs/microservices';
 
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const microserviceOptions: MicroserviceOptions = {
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBIT_MQ_URI],
+      queue: process.env.RABBIT_MQ_QUEUE,
+    },
+  };
+
+  const client: ClientProxy = ClientProxyFactory.create(microserviceOptions);
+
+  client.emit<any>('message_pattern', 'Hello RabbitMQ!');
+
   if (process.env.APP_ENV !== 'production') {
     app.enableCors({
       allowedHeaders: '*',
@@ -17,6 +36,8 @@ async function bootstrap() {
       origin: 'http://localhost:4200',
     });
   }
+
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
