9e38774288c613ff6acdb6e7e5dccb35f02b0a89
SDSamarasinghe <it20605902@my.sliit.lk>
2024-04-14 15:55:30 +0530

    Add RMQ module and send message to queue in RmqController

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { config } from 'dotenv';
+import { RmqModule } from './rmq/rmq.module';
 config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    RmqModule,
     ProductModule,
     ClientsModule.register([
       {
